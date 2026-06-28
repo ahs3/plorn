ddb7c1673a8f993b611fa2204baf910d4e6e914e
Al Stone <ahs3@ahs3.net>
2026-06-27 18:40:25 -0600

    Update copyright date in __init__.py
    Signed-off-by: Al Stone <ahs3@ahs3.net>

--- src/plorn/__init__.py
+++ src/plorn/__init__.py
@@ -1,5 +1,5 @@
 #######################################################################
-# Copyright (c) 2025, Albert H. Stone, III <ahs3@ahs3.net>
+# Copyright (c) 2026, Albert H. Stone, III <ahs3@ahs3.net>
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2025 Albert H. Stone, III <ahs3@ahs3.net>
 #######################################################################
